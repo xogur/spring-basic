b2816bd3d5afab92dbcd93d98dda242c9101679b
xogur <snsk656@naver.com>
2025-07-26 22:53:36 +0900

    13

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class LogDemoController {
 
+    //
     private final LogDemoService logDemoService;
     private final MyLogger myLogger;
 
